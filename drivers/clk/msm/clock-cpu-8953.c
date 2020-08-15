c8e87df67253eed961b391798031562a3a91e64a
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-08-16 05:03:04 +0530

    clk: msm: clock-{cpu,gcc}-8953: sync to ns-dbfs-mido
    clk: msm: clock-cpu-8953: use 435.2Mhz, 2.666GHz for min, max
    
    clk: msm: clock-{cpu,gcc}-8953: sync to ns-dbfs-mido

--- drivers/clk/msm/clock-cpu-8953.c
+++ drivers/clk/msm/clock-cpu-8953.c
@@ -132,15 +132,15 @@ static struct pll_clk apcs_hf_pll = {
 		.test_ctl_lo_val = 0x1C000000,
 	},
 	.base = &virt_bases[APCS_C0_PLL_BASE],
-	.max_rate = 2208000000UL,
-	.min_rate = 652800000UL,
+	.max_rate = 2828800000UL,
+	.min_rate = 435200000UL,
 	.src_rate =  19200000UL,
 	.c = {
 		.parent = &xo_a_clk.c,
 		.dbg_name = "apcs_hf_pll",
 		.ops = &clk_ops_variable_rate,
 		/* MX level of MSM is much higher than of PLL */
-		VDD_MX_HF_FMAX_MAP1(SVS, 2400000000UL),
+		VDD_MX_HF_FMAX_MAP1(SVS, 2828800000UL),
 		CLK_INIT(apcs_hf_pll.c),
 	},
 };
