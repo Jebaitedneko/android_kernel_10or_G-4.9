5545f92a9da2083b9598b1e02b99eb7f0251bb2c
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-10-03 00:13:43 +0530

    build: scripts: build_kernel: time the build

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,9 +7,15 @@ source anykernel/build_helper.sh
 [[ $1 == 'n' || $1 == 'dn'  ]] && nontreble
 [[ $2 == 's'                ]] && noslmk
 
+BUILD_START=$(date +"%s")
 make O=out ARCH=arm64 final_defconfig
 
 [[ $1 == 'n' || $1 == 't'   ]] && pcmake -j$(nproc --all)
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "\nBuild completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+
 [[ $1 == 'dt' || $1 == 'dn' ]] && pcmake dtbs
 [[ $1 == 'n'                ]] && pcmod
 
