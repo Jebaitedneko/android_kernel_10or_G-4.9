062768bae91cafb3c99daf5df26e6c0cfbeba028
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-10-30 02:08:11 +0530

    build: scripts: init build scripts

---
+++ build_kernel.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Do not touch this.
+
+source anykernel/build_helper.sh
+
+[[ $1 == 'n' || $1 == 'dn'  ]] && nontreble
+[[ $2 == 's'                ]] && noslmk
+[[ $2 == 'flto'             ]] && full_lto
+[[ $2 == 'tlto'             ]] && thin_lto
+
+BUILD_START=$(date +"%s")
+make O=out ARCH=arm64 final_defconfig
+
+[[ $1 == 'n' || $1 == 't'   ]] && pcmake -j$(nproc --all)
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "\nBuild completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+
+[[ $1 == 'dt' || $1 == 'dn' ]] && pcmake dtbs
+[[ $1 == 'n'                ]] && pcmod
+
+[[ $1 == 'n' || $1 == 't'   ]] && ./anykernel/build_zip.sh $1
+
+rm arch/arm64/configs/final_defconfig
