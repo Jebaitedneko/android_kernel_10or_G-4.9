017695121d34804f370e9d1dab17872762d5b147
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-11-17 19:23:02 +0530

    {scripts: fetch-latest-wireguard, net: netfilter: x_tables}: fix compile for vmalloc backport

--- scripts/fetch-latest-wireguard.sh
+++ scripts/fetch-latest-wireguard.sh
@@ -5,6 +5,8 @@ USER_AGENT="WireGuard-AndroidROMBuild/0.3 ($(uname -a))"
 exec 9>.wireguard-fetch-lock
 flock -n 9 || exit 0
 
+sed -i '/#define totalram_pages() totalram_pages/d' net/wireguard/compat/compat.h
+
 [[ $(( $(date +%s) - $(stat -c %Y "net/wireguard/.check" 2>/dev/null || echo 0) )) -gt 86400 ]] || exit 0
 
 while read -r distro package version _; do
