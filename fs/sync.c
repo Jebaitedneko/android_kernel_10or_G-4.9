c164ca3208d8cab676486477b6420db774100681
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-11-21 19:27:08 +0530

    fs: sync: don't log sync status

--- fs/sync.c
+++ fs/sync.c
@@ -133,7 +133,6 @@ static void do_sync_work(struct work_struct *work)
 	iterate_supers(sync_inodes_one_sb, &nowait);
 	iterate_supers(sync_fs_one_sb, &nowait);
 	iterate_bdevs(fdatawrite_one_bdev, NULL);
-	printk("Emergency Sync complete\n");
 	kfree(work);
 }
 
