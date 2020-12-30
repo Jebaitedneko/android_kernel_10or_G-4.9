9b1f76b79214fa4aca21d28a151ebb2e93869395
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-12-30 20:42:09 +0530

    build: introduce build scripts

---
+++ anykernel/build_zip.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+kernel_dir=.
+ak_dir=anykernel/anykernel3
+pronto_dir=anykernel/pronto_wlan
+image=$kernel_dir/out/arch/arm64/boot/Image.gz-dtb
+pre=` cat $kernel_dir/out/.config | grep Linux | cut -f 3 -d " "  `
+fmt=`date +%d\.%m\.%y_%r | sed 's/:/./g;s/ PM/.PM/g;s/ AM/.AM/g'`
+
+[ -d $ak_dir ] && echo -e "\nAnykernel 3 Present.\n" \
+|| mkdir -p $ak_dir \
+| git clone --depth=1 https://github.com/osm0sis/AnyKernel3 $ak_dir
+rm $ak_dir/anykernel.sh
+cp $ak_dir/../anykernel.sh $ak_dir
+
+cp -r $ak_dir ./ak_dir_working
+cp $image ./Image.gz-dtb
+mv Image.gz-dtb ./ak_dir_working && cd ak_dir_working
+
+zip -r ${pre}_MOCHI_${model}_${fmt}.zip . -x '*.git*' '*modules*' '*patch*' '*ramdisk*' 'LICENSE' 'README.md'
+mv *.zip ../out
+cd ..
+rm -rf ak_dir_working
