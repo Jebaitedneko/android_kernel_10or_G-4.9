ad47a99e5588967d58ecbe5726bba5b766e4aee0
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-12-30 20:42:09 +0530

    build: introduce build scripts

---
+++ anykernel/build_helper.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+[ -d out ] && rm -rf out && mkdir -p out || mkdir -p out
+
+TC_DIR=${HOME}/android/TOOLS/proton-clang
+CFG_DIR=$(pwd)/arch/arm64/configs
+CFG=$CFG_DIR/holland2_defconfig
+
+[ -d $TC_DIR ] \
+&& echo -e "\nProton-Clang Present.\n" \
+|| echo -e "\nProton-Clang Not Present. Downloading Around 500MB...\n" \
+| mkdir -p $TC_DIR \
+| git clone --depth=1 https://github.com/kdrag0n/proton-clang $TC_DIR \
+| echo "Done."
+
+echo -e "\nChecking Clang Version...\n"
+PATH="$TC_DIR/bin:${PATH}" clang --version
+echo -e "\n\nStarting Build...\n"
+
+cp $CFG $CFG_DIR/final_defconfig
+
+full_lto() {
+echo -e "\n\nSetting Full LTO Configs...\n\n"
+echo -e "\
+CONFIG_LTO_CLANG=y
+CONFIG_THINLTO=n
+" >> $CFG_DIR/final_defconfig
+}
+
+thin_lto() {
+full_lto
+echo -e "\n\nSetting Thin LTO Configs...\n\n"
+echo -e "\
+CONFIG_THINLTO=y
+" >> $CFG_DIR/final_defconfig
+}
+
+holland1() {
+echo -e "\n\nSetting holland1 Configs...\n\n"
+sed -i "\
+/CONFIG_ARCH_MSM8953/d
+/CONFIG_MACH_TENOR_G/d
+/CONFIG_MSM_ISPIF/d
+/CONFIG_PINCTRL_MSM8953/d
+" $CFG_DIR/final_defconfig
+echo -e "\
+CONFIG_ARCH_MSM8937=y
+CONFIG_IP_SCTP=y
+CONFIG_LEDS_QPNP_VIBRATOR=y
+CONFIG_MACH_TENOR_E=y
+CONFIG_MSMB_CAMERA_LEGACY=y
+CONFIG_MSM_ISPIF_V2=y
+CONFIG_PINCTRL_MSM8937=y
+CONFIG_QCOM_DCC=y
+CONFIG_SYN_COOKIES=y
+" >> $CFG_DIR/final_defconfig
+}
+
+mido() {
+echo -e "\n\nSetting mido Configs...\n\n"
+sed -i "\
+/CONFIG_FINGERPRINT_GF3258/d
+/CONFIG_MACH_TENOR_G/d
+/CONFIG_TOUCHSCREEN_GT9XX_v28/d
+" $CFG_DIR/final_defconfig
+echo -e "\
+CONFIG_F2FS_FS_ENCRYPTION=y
+CONFIG_F2FS_FS_SECURITY=y
+CONFIG_F2FS_FS=y
+CONFIG_FINGERPRINT_FPC1020_C6=y
+CONFIG_FINGERPRINT_GOODIX_GF3208_C6=y
+CONFIG_IR_MCE_KBD_DECODER=n
+CONFIG_IR_PWM=y
+CONFIG_LEDS_AW2013=y
+CONFIG_LIRC=y
+CONFIG_MACH_XIAOMI_C6=y
+CONFIG_MEDIA_RC_SUPPORT=y
+CONFIG_MSMB_CAMERA_LEGACY=y
+CONFIG_RC_DEVICES=y
+CONFIG_TOUCHSCREEN_GT9XX_v24_TOOL=y
+CONFIG_TOUCHSCREEN_GT9XX_v24_UPDATE=y
+CONFIG_TOUCHSCREEN_GT9XX_v24=y
+CONFIG_TOUCHSCREEN_IST3038C=y
+" >> $CFG_DIR/final_defconfig
+}
+
+pcmake() {
+PATH="$TC_DIR/bin:${PATH}" \
+make	\
+	O=out \
+	ARCH=arm64 \
+	CC="ccache clang" \
+	CXX="ccache clang++" \
+	AR="ccache llvm-ar" \
+	AS="ccache llvm-as" \
+	NM="ccache llvm-nm" \
+	LD="ccache ld.lld" \
+	STRIP="ccache llvm-strip" \
+	OBJCOPY="ccache llvm-objcopy" \
+	OBJDUMP="ccache llvm-objdump"\
+	OBJSIZE="ccache llvm-size" \
+	READELF="ccache llvm-readelf" \
+	HOSTCC="ccache clang" \
+	HOSTCXX="ccache clang++" \
+	HOSTAR="ccache llvm-ar" \
+	HOSTAS="ccache llvm-as" \
+	HOSTNM="ccache llvm-nm" \
+	HOSTLD="ccache ld.lld" \
+	CROSS_COMPILE="aarch64-linux-gnu-" \
+	CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+	CONFIG_DEBUG_SECTION_MISMATCH=y \
+	CONFIG_NO_ERROR_ON_MISMATCH=y \
+	$1 $2 $3 || exit
+}
---
+++ build_kernel.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Do not touch this.
+
+source anykernel/build_helper.sh
+
+[[ $2 == 'f' ]] && full_lto
+[[ $2 == 't' ]] && thin_lto
+
+[[ $1 == 'g'    ]] && export model=HOLLAND2
+[[ $1 == 'e'    ]] && holland1 && export model=HOLLAND1
+[[ $1 == 'm'    ]] && mido && export model=MIDO
+
+BUILD_START=$(date +"%s")
+
+make O=out ARCH=arm64 final_defconfig
+
+[[ $2 != 'dtb' ]] && pcmake -j$(nproc --all) || pcmake dtbs
+
+DIFF=$(($(date +"%s") - $BUILD_START))
+echo -e "\nBuild completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+
+[[ $2 != 'dtb' ]] && ./anykernel/build_zip.sh $1
+
+unset model
+rm arch/arm64/configs/final_defconfig
