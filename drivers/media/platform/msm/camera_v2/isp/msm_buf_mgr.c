384d2815fd2040081904ca18982c89b554819095
Jebaitedneko <Jebaitedneko@gmail.com>
2021-03-11 02:53:26 +0530

    Revert "camera_v2: isp: msm_buf_mgr: don't spam about invalid bufq"
    This reverts commit aadd723a0f7bfc2fd4a7938bc3991c8845f33861.

--- drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.c
@@ -875,8 +875,12 @@ static int msm_isp_buf_enqueue(struct msm_isp_buf_mgr *buf_mgr,
 	struct msm_isp_buffer *buf_info = NULL;
 
 	bufq = msm_isp_get_bufq(buf_mgr, info->handle);
-	if (!bufq)
+	if (!bufq) {
+		pr_err("%s: Invalid bufq, handle 0x%x, stream id %x num_plane %d\n"
+			, __func__, info->handle, (info->handle >> 8),
+			info->buffer.num_planes);
 		return -EINVAL;
+	}
 
 	buf_state = msm_isp_buf_prepare(buf_mgr, info, NULL);
 	if (buf_state < 0) {
