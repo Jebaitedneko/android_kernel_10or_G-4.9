518ec0170c3251bf411d5ec832f39f1eee720abe
Jebaitedneko <Jebaitedneko@gmail.com>
2021-03-14 02:00:13 +0530

    input: fingerprint: gf3258 (holland2): rewrite

--- drivers/input/fingerprint/gf3258/gf_spi.c
+++ drivers/input/fingerprint/gf3258/gf_spi.c
@@ -58,10 +58,6 @@ struct gf_dev {
 	unsigned users;
 	signed irq_gpio;
 	signed reset_gpio;
-	#ifdef CONFIG_MACH_TENOR_E
-	signed pwr_gpio;
-	signed id_gpio;
-	#endif
 	int irq;
 	int irq_enabled;
 };
@@ -82,26 +78,21 @@ static inline void sendnlmsg(char *message) {
 	if (!message || !nl_sk)
 		return;
 	skb_1 = alloc_skb(len, GFP_KERNEL);
-	if (!skb_1) {
-		pr_err("alloc_skb error\n");
+	if (!skb_1)
 		return;
-	}
 	slen = strlen(message);
 	nlh = nlmsg_put(skb_1, 0, 0, 0, MAX_MSGSIZE, 0);
-
 	NETLINK_CB(skb_1).portid = 0;
 	NETLINK_CB(skb_1).dst_group = 0;
-
 	message[slen] = '\0';
 	memcpy(NLMSG_DATA(nlh), message, slen+1);
-
 	netlink_unicast(nl_sk, skb_1, pid, MSG_DONTWAIT);
 }
 
 static inline void nl_data_ready(struct sk_buff *__skb) {
 	struct sk_buff *skb;
 	struct nlmsghdr *nlh;
-	char str[16];
+	char str[MAX_MSGSIZE];
 	skb = skb_get (__skb);
 	if (skb->len >= NLMSG_SPACE(0)) {
 		nlh = nlmsg_hdr(skb);
@@ -117,55 +108,33 @@ static inline void netlink_init(void) {
 	netlink_cfg.flags = 0;
 	netlink_cfg.input = nl_data_ready;
 	netlink_cfg.cb_mutex = NULL;
-
 	nl_sk = netlink_kernel_create(&init_net, NETLINK_TEST, &netlink_cfg);
-
-	if (!nl_sk)
-	pr_err("create netlink socket error\n");
 }
 
 static inline void netlink_exit(void) {
 	if (!nl_sk)
 		return;
-
 	netlink_kernel_release(nl_sk);
 	nl_sk = NULL;
 }
 
 static inline int gf_parse_dts(struct gf_dev* gf_dev) {
 	gf_dev->reset_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,"goodix,gpio_reset",0);
-	pr_info("gf::reset_gpio:%d\n", gf_dev->reset_gpio);
 	gpio_request(gf_dev->reset_gpio, "goodix_reset");
 	gpio_direction_output(gf_dev->reset_gpio, 1);
-
 	gf_dev->irq_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,"goodix,gpio_irq",0);
-	pr_info("gf::irq_gpio:%d\n", gf_dev->irq_gpio);
 	gpio_request(gf_dev->irq_gpio, "goodix_irq");
 	gpio_direction_input(gf_dev->irq_gpio);
-
 	return 0;
 }
 
 static inline void gf_cleanup(struct gf_dev	* gf_dev) {
-	pr_info("[info] %s\n",__func__);
 	if (gpio_is_valid(gf_dev->irq_gpio)) {
 		gpio_free(gf_dev->irq_gpio);
-		pr_info("remove irq_gpio success\n");
 	}
 	if (gpio_is_valid(gf_dev->reset_gpio)) {
 		gpio_free(gf_dev->reset_gpio);
-		pr_info("remove reset_gpio success\n");
 	}
-	#ifdef CONFIG_MACH_TENOR_E
-	if (gpio_is_valid(gf_dev->pwr_gpio)) {
-		gpio_free(gf_dev->pwr_gpio);
-		pr_info("remove pwr_gpio success\n");
-	}
-	if (gpio_is_valid(gf_dev->id_gpio)) {
-		gpio_free(gf_dev->id_gpio);
-		pr_info("remove id_gpio success\n");
-	}
-	#endif
 }
 
 static inline irqreturn_t gf_irq(int irq, void *handle) {
@@ -177,64 +146,48 @@ static inline irqreturn_t gf_irq(int irq, void *handle) {
 
 static inline int irq_setup(struct gf_dev *gf_dev) {
 	int status;
-
 	gf_dev->irq = gpio_to_irq(gf_dev->irq_gpio);
 	status = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
 			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
 			"gf", gf_dev);
-
-	if (status) {
-		pr_err("failed to request IRQ:%d\n", gf_dev->irq);
+	if (status)
 		return status;
-	}
-	enable_irq_wake(gf_dev->irq);
-	gf_dev->irq_enabled = 1;
-
+	if(!gf_dev->irq_enabled)
+		enable_irq_wake(gf_dev->irq);
+		gf_dev->irq_enabled = 1;
 	return status;
 }
 
 static inline void irq_cleanup(struct gf_dev *gf_dev) {
-	gf_dev->irq_enabled = 0;
-	disable_irq_wake(gf_dev->irq);
-	free_irq(gf_dev->irq, gf_dev);
+	if(gf_dev->irq_enabled)
+		disable_irq_wake(gf_dev->irq);
+		disable_irq(gf_dev->irq);
+		free_irq(gf_dev->irq, gf_dev);
+		gf_dev->irq_enabled = 0;
 }
 
 static inline long gf_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
 	struct gf_dev *gf_dev = &gf;
 	int retval = 0;
 	u8 netlink_route = NETLINK_TEST;
-
-	if (_IOC_TYPE(cmd) != GF_IOC_MAGIC)
-		return -ENODEV;
-
-	if (_IOC_DIR(cmd) & _IOC_READ)
-		retval = !access_ok(VERIFY_WRITE, (void __user *)arg, _IOC_SIZE(cmd));
-	else if (_IOC_DIR(cmd) & _IOC_WRITE)
-		retval = !access_ok(VERIFY_READ, (void __user *)arg, _IOC_SIZE(cmd));
-	if (retval)
-		return -EFAULT;
-
 	switch (cmd) {
 		case GF_IOC_INIT:
-			pr_debug("%s GF_IOC_INIT\n", __func__);
 			if (copy_to_user((void __user *)arg, (void *)&netlink_route, sizeof(u8))) {
-				pr_err("GF_IOC_INIT failed\n");
 				retval = -EFAULT;
 				break;
 			}
 			break;
 		case GF_IOC_DISABLE_IRQ:
-			pr_debug("%s GF_IOC_DISABLE_IRQ\n", __func__);
+			if(gf_dev->irq_enabled)
+				disable_irq(gf_dev->irq);
 				gf_dev->irq_enabled = 0;
-				disable_irq_wake(gf_dev->irq);
 			break;
 		case GF_IOC_ENABLE_IRQ:
-			pr_debug("%s GF_IOC_ENABLE_IRQ\n", __func__);
-				enable_irq_wake(gf_dev->irq);
+			if(!gf_dev->irq_enabled)
+				enable_irq(gf_dev->irq);
 				gf_dev->irq_enabled = 1;
 			break;
 		case GF_IOC_RESET:
-			pr_debug("%s GF_IOC_RESET\n", __func__);
 				gpio_direction_output(gf_dev->reset_gpio, 1);
 				gpio_set_value(gf_dev->reset_gpio, 0);
 				mdelay(3);
@@ -244,28 +197,22 @@ static inline long gf_ioctl(struct file *filp, unsigned int cmd, unsigned long a
 		default:
 			break;
 	}
-
 	return retval;
 }
 
 static inline int gf_open(struct inode *inode, struct file *filp) {
 	struct gf_dev *gf_dev = &gf;
 	int status = -ENXIO;
-
 	list_for_each_entry(gf_dev, &device_list, device_entry) {
 		if (gf_dev->devt == inode->i_rdev) {
-			pr_info("Found\n");
 			status = 0;
 			break;
 		}
 	}
-
 	if (status == 0) {
 		gf_dev->users++;
 		filp->private_data = gf_dev;
 		nonseekable_open(inode, filp);
-		pr_info("Succeed to open device. irq = %d\n",
-				gf_dev->irq);
 		if (gf_dev->users == 1) {
 			status = gf_parse_dts(gf_dev);
 			if (status)
@@ -280,29 +227,20 @@ static inline int gf_open(struct inode *inode, struct file *filp) {
 		mdelay(3);
 		gpio_set_value(gf_dev->reset_gpio, 1);
 		mdelay(3);
-	} else {
-		pr_info("No device for minor %d\n", iminor(inode));
 	}
-
 	return status;
 }
 
 static inline int gf_release(struct inode *inode, struct file *filp) {
 	struct gf_dev *gf_dev = &gf;
 	int status = 0;
-
 	gf_dev = filp->private_data;
 	filp->private_data = NULL;
-
-	/*last close?? */
 	gf_dev->users--;
 	if (!gf_dev->users) {
-		pr_info("disble_irq. irq = %d\n", gf_dev->irq);
 		irq_cleanup(gf_dev);
 		gf_cleanup(gf_dev);
-		pr_info("---- power off ----\n");
 	}
-
 	return status;
 }
 
@@ -318,30 +256,21 @@ static inline int gf_probe(struct platform_device *pdev) {
 	struct gf_dev *gf_dev = &gf;
 	int status = -EINVAL;
 	unsigned long minor;
-
 	INIT_LIST_HEAD(&gf_dev->device_entry);
 	gf_dev->spi = pdev;
 	gf_dev->irq_gpio = -EINVAL;
 	gf_dev->reset_gpio = -EINVAL;
-	#ifdef CONFIG_MACH_TENOR_E
-	gf_dev->pwr_gpio = -EINVAL;
-	gf_dev->id_gpio = -EINVAL;
-	#endif
-
 	minor = find_first_zero_bit(minors, N_SPI_MINORS);
 	if (minor < N_SPI_MINORS) {
 		struct device *dev;
-
 		gf_dev->devt = MKDEV(SPIDEV_MAJOR, minor);
 		dev = device_create(gf_class, &gf_dev->spi->dev, gf_dev->devt,
 				gf_dev, GF_DEV_NAME);
 		status = IS_ERR(dev) ? PTR_ERR(dev) : 0;
 	} else {
-		dev_dbg(&gf_dev->spi->dev, "no minor number available!\n");
 		status = -ENODEV;
 		return status;
 	}
-
 	if (status == 0) {
 		set_bit(minor, minors);
 		list_add(&gf_dev->device_entry, &device_list);
@@ -349,41 +278,32 @@ static inline int gf_probe(struct platform_device *pdev) {
 		gf_dev->devt = 0;
 		return status;
 	}
-
 	gf_dev->input = input_allocate_device();
 	if (gf_dev->input == NULL) {
-		pr_err("%s, failed to allocate input device\n", __func__);
 		status = -ENOMEM;
 		if (gf_dev->input != NULL)
 			input_free_device(gf_dev->input);
 	}
-
 	gf_dev->input->name = GF_INPUT_NAME;
 	status = input_register_device(gf_dev->input);
 	if (status) {
-		pr_err("failed to register input device\n");
 		if (gf_dev->devt != 0) {
-			pr_info("Err: status = %d\n", status);
 			list_del(&gf_dev->device_entry);
 			device_destroy(gf_class, gf_dev->devt);
 			clear_bit(MINOR(gf_dev->devt), minors);
 		}
 	}
-
 	return status;
 }
 
 static inline int gf_remove(struct platform_device *pdev) {
 	struct gf_dev *gf_dev = &gf;
-
 	if (gf_dev->input)
 		input_unregister_device(gf_dev->input);
 	input_free_device(gf_dev->input);
-
 	list_del(&gf_dev->device_entry);
 	device_destroy(gf_class, gf_dev->devt);
 	clear_bit(MINOR(gf_dev->devt), minors);
-
 	return 0;
 }
 
@@ -404,45 +324,23 @@ static struct platform_driver gf_driver = {
 
 static inline int __init gf_init(void) {
 	int status;
-
-	#ifdef CONFIG_MACH_TENOR_E
-	struct gf_dev *gf_dev = &gf;
-	gf_dev->pwr_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node, "goodix,gpio_pwr",0);
-	gpio_request(gf_dev->pwr_gpio, "goodix_pwr");
-	gpio_direction_output(gf_dev->pwr_gpio, 1);
-	gpio_set_value(gf_dev->pwr_gpio, 0);
-	mdelay(10);
-	gpio_set_value(gf_dev->pwr_gpio, 1);
-	mdelay(10);
-
-	gf_dev->id_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node, "goodix,gpio_id",0);
-	gpio_request(gf_dev->id_gpio, "goodix_id");
-	gpio_direction_input(gf_dev->id_gpio);
-	#endif
-
 	BUILD_BUG_ON(N_SPI_MINORS > 256);
 	status = register_chrdev(SPIDEV_MAJOR, CHRD_DRIVER_NAME, &gf_fops);
 	if (status < 0) {
-		pr_warn("Failed to register char device!\n");
 		return status;
 	}
 	SPIDEV_MAJOR = status;
 	gf_class = class_create(THIS_MODULE, CLASS_NAME);
 	if (IS_ERR(gf_class)) {
 		unregister_chrdev(SPIDEV_MAJOR, gf_driver.driver.name);
-		pr_warn("Failed to create class.\n");
 		return PTR_ERR(gf_class);
 	}
 	status = platform_driver_register(&gf_driver);
 	if (status < 0) {
 		class_destroy(gf_class);
 		unregister_chrdev(SPIDEV_MAJOR, gf_driver.driver.name);
-		pr_warn("Failed to register SPI driver.\n");
 	}
-
 	netlink_init();
-
-	pr_info("status = 0x%x\n", status);
 	return 0;
 }
 module_init(gf_init);
